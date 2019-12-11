de95720e91a3a9ee5dcf5230fefc58b364bfd863
Gerhard Pegel <gpegel@gmail.com>
2019-12-11 21:32:36 +0100

    solved day3 part1

--- Day3/Day3_challenge.py
+++ Day3/Day3_challenge.py
@@ -1,8 +1,10 @@
-# sampple code to test
-
-import math
-p1 = [4, 0]
-p2 = [6, 6]
-distance = math.sqrt( ((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2) )
-
-print(distance)
+def generate_wire(instr_line):
+    current_pos = [0, 0]
+    for instr in instr_line.split(','):
+        for _ in range(int(instr[1:])):
+            current_pos[0 if instr[0] in ('L', 'R') else 1] += -1 if instr[0] in ('L', 'D') else 1
+            yield tuple(current_pos)
+with open('input', 'r') as f:
+    wires = [list(process_wire(line)) for line in f.readlines()]
+intersections = set(wires[0]) & set(wires[1])
+print(min(abs(x)+abs(y) for (x, y) in intersections))
